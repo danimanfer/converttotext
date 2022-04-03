b31c8e9eaffc31a75c66530dc328d39f96b0a607
Daniel Fernandes <dmanfer@gmail.com>
2022-04-03 18:04:40 -0300

    heroku tesseract

--- src/main/java/br/com/dmf/textconvert/service/ConvetToTextService.java
+++ src/main/java/br/com/dmf/textconvert/service/ConvetToTextService.java
@@ -1,15 +1,9 @@
 package br.com.dmf.textconvert.service;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import org.springframework.stereotype.Service;
 
-import br.com.dmf.textconvert.utils.CopyDirectoryUtils;
 import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
@@ -19,33 +13,34 @@ public class ConvetToTextService {
 
 	public String gerararquivo(String arquivo, String diretorio) {
 
-		String dirUser = System.getProperty("user.dir");
+//		String dirUser = System.getProperty("user.dir");
 
-		String tessdata = dirUser + "/tessdata";
+//		String tessdata = dirUser + "/tessdata";
+		String tessdata = "/.apt/usr/share/tesseract-ocr/4.00/tessdata";
 		
-		Path path = Paths.get(tessdata);
+//		Path path = Paths.get(tessdata);
 		
-		if (!Files.isDirectory(path)) {
-			
-			try {
-				Files.createDirectories(path);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-			String data1 = "/eng.traineddata";
-			InputStream in=	getClass().getResourceAsStream("/tessdata"+data1);
-			CopyDirectoryUtils.copy(in, tessdata+ data1);
-			
-			String data2 = "/osd.traineddata";
-			InputStream in2 =	getClass().getResourceAsStream("/tessdata"+data2);
-			CopyDirectoryUtils.copy(in2, tessdata+ data2);
-			
-			String data3 = "/por.traineddata";
-			InputStream in3=	getClass().getResourceAsStream("/tessdata"+data3);
-			CopyDirectoryUtils.copy(in3, tessdata+ data3);
-		}
+//		if (!Files.isDirectory(path)) {
+//			
+//			try {
+//				Files.createDirectories(path);
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//
+//			String data1 = "/eng.traineddata";
+//			InputStream in=	getClass().getResourceAsStream("/tessdata"+data1);
+//			CopyDirectoryUtils.copy(in, tessdata+ data1);
+//			
+//			String data2 = "/osd.traineddata";
+//			InputStream in2 =	getClass().getResourceAsStream("/tessdata"+data2);
+//			CopyDirectoryUtils.copy(in2, tessdata+ data2);
+//			
+//			String data3 = "/por.traineddata";
+//			InputStream in3=	getClass().getResourceAsStream("/tessdata"+data3);
+//			CopyDirectoryUtils.copy(in3, tessdata+ data3);
+//		}
 		
 		File image = new File(arquivo);
 
